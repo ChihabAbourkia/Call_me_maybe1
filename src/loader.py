76bd9b48d07c9779dfdcbee0f910c7f47bb0d5f0
ChihabAbourkia <abourkiac@gmail.com>
2026-06-16 22:06:31 +0100

    function_loader

--- src/loader.py
+++ src/loader.py
@@ -1,8 +1,13 @@
 import json
-from .json_validator import Prompts
+from .json_validator import Prompts ,Functions
 
 def prompts_loader(path):
     with open(path, "r") as file:
         data = json.load(file)
     return [Prompts(**item) for item in data]
 
+
+def function_loader(path):
+    with open(path, "r") as file:
+        data = json.load(file)
+    return [Functions(**item) for item in data]
