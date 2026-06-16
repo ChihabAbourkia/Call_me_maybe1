a3396062aef744e6127fbdfdbf15a584268610b3
ChihabAbourkia <abourkiac@gmail.com>
2026-06-16 22:19:23 +0100

    errors handling

--- src/__main__.py
+++ src/__main__.py
@@ -4,10 +4,14 @@ def main():
     print("🤖 Starting LLM fuction calling system....")
     parser = argparser()
 
-    print("Loading pompts and functions difinetion")
+    print("📂 Loading Functions and promts...")
+    functions = function_loader(parser.functions_definition)
+    if not functions:
+        raise RuntimeError("No function difinetion found...")
     prompts = prompts_loader(parser.input)
-    function  = function_loader(parser.functions_definition)
-    
+    if not prompts:
+        raise RuntimeError("No input prompts found. Please provide at least one prompt.")
+
 
 if __name__ == "__main__":
     main()
